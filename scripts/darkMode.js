78fd09f6615b112867ba139cb001a96092f4cf83
Paul Amago <apaulo2035@gmail.com>
2023-10-06 21:50:02 -0600

    finalize on the start of chamber proj

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,11 +1,15 @@
-// const modeButton = document.querySelector("#mode");
-// const main = document.querySelector(".light-mode");
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector(".light-mode");
+const regularTextColor = document.querySelectorAll(".text-color");
+const subtextColor = document.querySelectorAll(".subtext");
+const titleColor = document.querySelector(".title");
 
-// modeButton.addEventListener("click", () => {
-//   if (modeButton.textContent.includes("☑️")) {
-//     main.classList.toggle("light-mode");
-//     modeButton.textContent = "❎";
-//   } else {
-//     modeButton.textContent = "☑️";
-//   }
-// });
+modeButton.addEventListener("click", () => {
+  if (modeButton.textContent.includes("☑️")) {
+    main.classList.toggle("light-mode");
+    titleColor.classList.toggle("title");
+    modeButton.textContent = "❎";
+  } else {
+    modeButton.textContent = "☑️";
+  }
+});
