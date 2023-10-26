4a887f623c564fafa99aab17b1aa7caf15382d8e
Paul Amago <apaulo2035@gmail.com>
2023-10-26 09:30:56 -0600

    hamburger button

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -3,5 +3,5 @@ const navigation = document.querySelector(".grid-container");
 
 hamButton.addEventListener("click", () => {
   navigation.classList.toggle("show");
-  hamButton.classList.toggle("show");
+  hamButton.classList.toggle("open");
 });
