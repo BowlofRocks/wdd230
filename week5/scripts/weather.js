5bcb6fbb46b4e1ac2ddb512b4aa8a38d4894a2f3
Paul Amago <apaulo2035@gmail.com>
2023-11-14 08:58:08 -0700

    weather api

---
+++ week5/scripts/weather.js
@@ -0,0 +1,32 @@
+const currentTemp = document.querySelector("#current-temp");
+const weatherTemp = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?lat=49.75048745425214&lon=6.638804777950605&units=imperial&appid=4ad8a947df6bc0535bf9d31e780a8afc";
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp} &deg;F`;
+  const inconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute("src", inconsrc);
+  weatherIcon.setAttribute("alt", desc);
+  captionDesc.textContent = `${desc}`;
+}
+
+apiFetch();
