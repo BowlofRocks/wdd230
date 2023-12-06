6abec1625cab220ca461f3a5d860bd7bb96cf3df
Paul Amago <apaulo2035@gmail.com>
2023-12-05 21:17:41 -0700

    home page weather

--- scripts/links.js
+++ scripts/links.js
@@ -3,12 +3,44 @@ const linksURL = "https://bowlofrocks.github.io/wdd230/data/links.json";
 
 async function getLinks() {
   const response = await fetch(linksURL);
-  const data = await response.json();
-  console.log(data);
-}
-
-function displayLinks(weeks){
-  weeks.foreach(week) => console.log(*)
+  try {
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      displayLinks(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 getLinks();
+
+function displayLinks(data) {
+  console.log(data);
+  let linkElement = document.querySelector("#links");
+  linkElement.innerHTML = "";
+
+  data.weeks.forEach((week) => {
+    let links = week.links;
+    console.log(links);
+    let weekNumber = week.week;
+    console.log(weekNumber);
+
+    let li = document.createElement("li");
+    li.textContent = `${weekNumber} `;
+
+    links.forEach((link) => {
+      let url = link.url;
+      let title = link.title;
+      let anchor = document.createElement("a");
+
+      anchor.setAttribute("href", url);
+      anchor.textContent = `${title} || `;
+      li.appendChild(anchor);
+    });
+    linkElement.appendChild(li);
+  });
+}
