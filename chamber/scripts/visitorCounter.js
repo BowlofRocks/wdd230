1d6ace400d979797b8fb253f2c12d7045af05892
Paul Amago <apaulo2035@gmail.com>
2023-12-09 18:27:32 -0700

    finished

--- chamber/scripts/visitorCounter.js
+++ chamber/scripts/visitorCounter.js
@@ -2,6 +2,14 @@ let currentTime = new Date().getTime;
 let lastVisitedTime = localStorage.getItem("lastVisited");
 let visitorMessage = "";
 
+const currentDay = new Date(Date.now()).getDay();
+console.log(currentDay);
+
+if (currentDay < 4) {
+  banner = "Come attend our Meet and Greets at 7:00p.m Central!";
+  document.querySelector("#banner-message").textContent = banner;
+}
+
 if (lastVisitedTime) {
   let timeDifference = currentTime - lastVisitedTime;
   let dayDifference = timeDifference / (1000 * 60 * 60 * 24);
@@ -14,6 +22,7 @@ if (lastVisitedTime) {
 } else {
   message = "Welcome! Let us know if you have any questions";
 }
+
 document.querySelector("#visitor-message").textContent = message;
 
 localStorage.setItem("lastVisited", currentTime);
